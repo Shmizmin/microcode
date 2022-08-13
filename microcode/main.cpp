758b4ae5be2c753200b2accdb0d028e4aae6623b
Connor Link <connorlink248@gmail.com>
2022-08-12 23:30:24 -0500

    Update microcode to reject latest CPU hardware changes.

--- microcode/main.cpp
+++ microcode/main.cpp
@@ -61,11 +61,28 @@
  */
 
 #define BIT(x) (1ull << x##ull)
+#define IMM(x) (x << 56ull)
+
 
 enum : std::uint64_t
 {
+    IMMEDIATE_IN = BIT(55),
+    
+
+    
+    JUMP_GREATER_THAN_ZERO = BIT(45),
+    JUMP_CARRY_SET         = BIT(44),
+    JUMP_ON_ZERO           = BIT(43),
+    
+    PCU_INSTRUCTION_REGISTER_IN = BIT(42),
+    
+    RF_TEMP_OUT = BIT(41),
+    RF_TEMP_IN  = BIT(40),
+    
+    CONNECT_F_TO_DATA_BUS = BIT(39),
+    
     PC_LOAD_VALUE               = BIT(38),
-    PC_ENABLE                   = BIT(37),
+    PC_ENABLE                   = BIT(37), //should be unused
     PC_SKIP_TO_NEXT_INSTRUCTION = BIT(36),
     PC_ADDRESS_BUS_OUT          = BIT(35),
     PC_ADDRESS_BUS_IN           = BIT(34),
@@ -112,62 +129,211 @@ enum : std::uint64_t
     
 };
 
+
+static constexpr const auto FETCH = PC_ADDRESS_BUS_OUT | LSU_ROM_ENABLE | LSU_READ_ENABLE | PCU_INSTRUCTION_REGISTER_IN;
+
+
+
+std::array<std::uint64_t, 8> emitMvb(std::uint64_t dest_in, std::uint64_t src_out)
+{
+    return
+    {
+        FETCH,
+        dest_in | src_out,
+        PC_SKIP_TO_NEXT_INSTRUCTION,
+        0ull,
+        0ull,
+        0ull,
+        0ull,
+        0ull,
+    };
+}
+
+std::array<std::uint64_t, 8> emitAlu2Arg(std::uint64_t dest_in, std::uint64_t dest_out, std::uint64_t src_out, std::uint64_t op)
+{
+    return
+    {
+        FETCH,
+        dest_out | ALU_WRITE_A | ALU_IN,
+        src_out  | ALU_WRITE_B | ALU_IN,
+        RF_FLAGS_OUT | ALU_WRITE_F | ALU_IN,
+        op | ALU_IN | ALU_WRITE_OUT,
+        ALU_OUT | dest_in,
+        PC_SKIP_TO_NEXT_INSTRUCTION,
+        0ull,
+    };
+}
+
+
+
+
+
 int main(int argc, const char** argv)
 {
     switch (argc)
     {
-        case 3:
+        case 2:
         {
-            std::string_view command = argv[1], path = argv[2];
+            std::string_view path = argv[1];
             
             std::ofstream file(path, std::ios::out | std::ios::binary);
             
             if (file.good())
             {
-                if (command == std::string_view("-c"))
-                {
-                    struct operation
-                    {
-                        std::array<std::uint64_t, 8> sequence;
-                    };
-                    
-                    std::array<operation, std::numeric_limits<std::uint8_t>::max()> microcode;
-                    
-                    //fetch
-                    microcode[NULL] = { 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u };
-                    
-                    microcode[NOP] = { 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u };
-                    
-                    //brk impossible
-                    
-                    microcode[ADC_A_B] =
-                    {
-                        RF_A_OUT | ALU_WRITE_A | ALU_IN,
-                        RF_B_OUT | ALU_WRITE_B | ALU_IN,
-                        RF_FLAGS_OUT | ALU_WRITE_F | ALU_IN,
-                        ALU_ADDITION | ALU_IN,
-                        ALU_ADDITION | ALU_IN | ALU_WRITE_OUT,
-                        ALU_OUT | RF_A_IN,
-                        
-                        
-                    };
-                    
-                    //file.write(buffer.data(), buffer.size() * sizeof(*buffer.data()));
-                }
-                else
-                {
-                    std::fprintf(stderr, "Invalid command\n");
-                    return EXIT_FAILURE;
-                }
+                std::array<std::array<std::uint64_t, 8>, std::numeric_limits<std::uint8_t>::max() + 1> µcode{ 0 };
                 
                 
+                
+                µcode[MVB_A_B] = emitMvb(RF_A_IN, RF_B_OUT);
+                µcode[MVB_A_C] = emitMvb(RF_A_IN, RF_C_OUT);
+                µcode[MVB_A_D] = emitMvb(RF_A_IN, RF_D_OUT);
+                
+                µcode[MVB_B_A] = emitMvb(RF_B_IN, RF_A_OUT);
+                µcode[MVB_B_C] = emitMvb(RF_B_IN, RF_C_OUT);
+                µcode[MVB_B_D] = emitMvb(RF_B_IN, RF_D_OUT);
+                
+                µcode[MVB_C_A] = emitMvb(RF_C_IN, RF_A_OUT);
+                µcode[MVB_C_B] = emitMvb(RF_C_IN, RF_B_OUT);
+                µcode[MVB_C_D] = emitMvb(RF_C_IN, RF_D_OUT);
+                
+                µcode[MVB_D_A] = emitMvb(RF_D_IN, RF_A_OUT);
+                µcode[MVB_D_B] = emitMvb(RF_D_IN, RF_B_OUT);
+                µcode[MVB_D_C] = emitMvb(RF_C_IN, RF_C_OUT);
+                
+                
+                
+                
+                //fetch
+                µcode[0] =
+                {
+                    //PC_ADDRESS_BUS_OUT | LSU_ROM_ENABLE | LSU_READ_ENABLE,
+                    0u,
+                    0u,
+                    0u,
+                    0u,
+                    
+                    0u,
+                    0u,
+                    0u,
+                    0u
+                };
+                
+                µcode[NOP] = { 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u };
+                
+                //brk impossible
+                µcode[BRK] =
+                {
+                    IMM(static_cast<std::uint64_t>(0b0001000)) | IMMEDIATE_IN | CONNECT_F_TO_DATA_BUS | RF_FLAGS_IN,
+                    PC_SKIP_TO_NEXT_INSTRUCTION,
+                    0ull,
+                    0ull,
+                    
+                    0ull,
+                    0ull,
+                    0ull,
+                    0ull
+                };
+                
+                µcode[ADC_A_B] =
+                {
+                    RF_A_OUT | ALU_WRITE_A | ALU_IN,
+                    RF_B_OUT | ALU_WRITE_B | ALU_IN,
+                    RF_FLAGS_OUT | ALU_WRITE_F | ALU_IN,
+                    ALU_ADDITION | ALU_IN | ALU_WRITE_OUT,
+                    ALU_OUT | RF_A_IN,
+                    PC_SKIP_TO_NEXT_INSTRUCTION,
+                    0ull,
+                    0ull,
+                };
+                
+                µcode[MVB_B_A] =
+                {
+                    RF_B_OUT | RF_A_IN,
+                    PC_SKIP_TO_NEXT_INSTRUCTION,
+                    0ull,
+                    0ull,
+                    
+                    0ull,
+                    0ull,
+                    0ull,
+                    0ull
+                };
+                
+                
+                //jnz
+                //and Flags with zero bit
+                //load temp reg with 255
+                
+    
+                auto counter = 0;
+                
+            
+                for (auto i = 0; i < µcode.size(); ++i)
+                {
+                    for (auto j = 0; j < µcode[0].size(); ++j)
+                    {
+                        auto µop = µcode[i][j];
+                        
+                        counter++;
+                        std::fprintf(stderr, "Counter: %i\n", counter);
+                        
+                        std::uint32_t hi = ((µop & 0xFFFFFFFF00000000) >> 32);
+                        std::uint32_t lo = ((µop & 0x00000000FFFFFFFF) >> 0);
+                        
+                        std::uint16_t hi_hi = ((hi & 0xFFFF0000) >> 16);
+                        std::uint16_t hi_lo = ((hi & 0x0000FFFF) >> 0);
+                        
+                        std::uint16_t lo_hi = ((lo & 0xFFFF0000) >> 16);
+                        std::uint16_t lo_lo = ((lo & 0x0000FFFF) >> 0);
+                        
+                        
+                        
+
+                        std::uint8_t hi_hi_hi = ((hi_hi & 0xFF00) >> 8);
+                        std::uint8_t hi_hi_lo = ((hi_hi & 0x00FF) >> 0);
+                        
+                        std::uint8_t hi_lo_hi = ((hi_lo & 0xFF00) >> 8);
+                        std::uint8_t hi_lo_lo = ((hi_lo & 0x00FF) >> 0);
+                        
+                        
+                        std::uint8_t lo_hi_hi = ((lo_hi & 0xFF00) >> 8);
+                        std::uint8_t lo_hi_lo = ((lo_hi & 0x00FF) >> 0);
+                        
+                        std::uint8_t lo_lo_hi = ((lo_lo & 0xFF00) >> 8);
+                        std::uint8_t lo_lo_lo = ((lo_lo & 0x00FF) >> 0);
+                        
+                        using b = std::uint8_t;
+                        
+#define WRITE(x) file.write(reinterpret_cast<char*>(&x), sizeof(x))
+                        
+                        
+                        WRITE(lo_lo_hi);
+                        WRITE(lo_lo_lo);
+                        
+                        WRITE(lo_hi_hi);
+                        WRITE(lo_hi_lo);
+                        
+                        WRITE(hi_lo_hi);
+                        WRITE(hi_lo_lo);
+                        
+                        WRITE(hi_hi_hi);
+                        WRITE(hi_hi_lo);
+                        
+                        
+                        
+                        
+                        //file.write(reinterpret_cast<char*>(&µop), sizeof(µop));
+                    }
+                }
+                
+                //file.write(buffer.data(), buffer.size() * sizeof(*buffer.data()));
             }
             else
             {
                 std::fprintf(stderr, "Could not open file %s for writing\n", path.data());
                 return EXIT_FAILURE;
             }
-        }
+        } break;
             
         default:
             std::fprintf(stderr, "Invalid number of arguments specified\n");
